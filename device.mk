34afa7fdb2ca546a0929a74461f1e640336723ca
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2017-07-25 20:55:45 -0500

    update

--- device.mk
+++ device.mk
@@ -237,8 +237,10 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8937 \
-    gps.conf
+    gps.msm8937
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
 
 # Misc dependency packages
 PRODUCT_PACKAGES += \
